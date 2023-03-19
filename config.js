fe4592438f63dbcead7342ba7a2ef42613a9fee1
demongthan <nam.nguyenbku00@gmail.com>
2023-03-19 08:43:05 +0700

    nam commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Thịnh';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Mừng sinh thần thằng em 19/3/2022';
+const titleCard = 'Tặng thằng em';
+const contentCard = 'Nhân ngày sinh nhật, anh chúc chú mày luôn luôn khỏe mạnh, hạnh phúc. Luôn đạt được những mục tiêu mình đề ra.';
+
+const giftImage = 'thinh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
